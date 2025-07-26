c5d7295461eee8915c6e9853437b82a01f582a14
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2025-07-26 18:58:25 -0300

    sudoku na forma textual

--- src/main/java/br/com/dio/model/GameStatusEnum.java
+++ src/main/java/br/com/dio/model/GameStatusEnum.java
@@ -1,4 +1,19 @@
 package br.com.dio.model;
 
-public class GameStatusEnum {
+public enum GameStatusEnum {
+
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
 }
